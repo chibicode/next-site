7ed188232d1abfb6279460557d3081d55149b4cd
Shu Uesugi <shu@chibicode.com>
2020-01-14 23:50:25 -0800

    Add sidebar

---
+++ components/examples/sidebar-routes.js
@@ -0,0 +1,70 @@
+import { useRouter } from 'next/router';
+import { getCategoryPath } from '../../lib/docs/utils';
+import { Post, Category, Heading } from '../sidebar';
+
+export function getSlug({ slug }) {
+  return `/examples/${slug.join('/')}`;
+}
+
+function SidebarRoutes({ isMobile, routes: currentRoutes, level = 1 }) {
+  const router = useRouter();
+  const slug = router.pathname === '/examples' ? '/examples' : getSlug(router.query);
+
+  return (
+    <>
+      {currentRoutes.map(({ path, title, routes, heading, introduction, open }) => {
+        if (introduction) {
+          return (
+            <Post
+              key="introduction"
+              isMobile={isMobile}
+              level={level}
+              route={{
+                href: '/examples',
+                pathname: '/examples',
+                selected: router.pathname === '/examples',
+                title: 'Introduction',
+                path: '/examples'
+              }}
+            />
+          );
+        }
+
+        if (routes) {
+          const pathname = getCategoryPath(routes);
+          const selected = slug.startsWith(pathname);
+          const opened = selected || isMobile ? false : open;
+
+          if (heading) {
+            return (
+              <Heading key={title} title={title}>
+                {pathname}
+                <SidebarRoutes isMobile={isMobile} routes={routes} level={level + 1} />
+              </Heading>
+            );
+          }
+
+          return (
+            <Category
+              key={pathname}
+              level={level}
+              title={title}
+              selected={selected}
+              opened={opened}
+            >
+              <SidebarRoutes isMobile={isMobile} routes={routes} level={level + 1} />
+            </Category>
+          );
+        }
+
+        const href = '/examples/[...slug]';
+        const selected = slug.startsWith(path);
+        const route = { href, title, pathname: path, selected };
+
+        return <Post key={title} isMobile={isMobile} level={level} route={route} />;
+      })}
+    </>
+  );
+}
+
+export default SidebarRoutes;
---
+++ pages/examples/[...slug].js
@@ -0,0 +1,64 @@
+import { useRouter } from 'next/router';
+import Page from '../../components/page';
+import PageContent from '../../components/page-content';
+import Header from '../../components/header';
+import Navbar from '../../components/navbar';
+import Container from '../../components/container';
+import SocialMeta from '../../components/social-meta';
+import { Sidebar, SidebarMobile } from '../../components/sidebar';
+import manifest from '../../lib/examples/manifest';
+import SidebarRoutes from '../../components/examples/sidebar-routes';
+
+const Examples = ({ routes }) => {
+  const title = `Examples | Next.js`;
+  const description = 'TBD';
+  const router = useRouter();
+  const { asPath } = router;
+
+  return (
+    <Page title={title} description={false}>
+      <PageContent>
+        <Header height={{ desktop: 64, mobile: 114 }} shadow defaultActive>
+          <Navbar />
+          <SidebarMobile>
+            <SidebarRoutes isMobile routes={routes} />
+          </SidebarMobile>
+        </Header>
+        <Container>
+          <div className="content">
+            <Sidebar fixed>
+              <SidebarRoutes routes={routes} />
+            </Sidebar>
+          </div>
+        </Container>
+        <SocialMeta
+          title={title}
+          url={`https://nextjs.org${asPath}`}
+          image="/static/twitter-cards/examples.png"
+          description={description}
+        />
+      </PageContent>
+      <style jsx>{`
+        .content {
+          display: flex;
+          margin-top: 2rem;
+          margin-bottom: 5rem;
+        }
+        /* Remove the top margin of the first heading in the sidebar */
+        :global(.heading:first-child > h4) {
+          margin-top: 0;
+        }
+      `}</style>
+    </Page>
+  );
+};
+
+export async function unstable_getStaticProps() {
+  return {
+    props: {
+      routes: manifest.routes
+    }
+  };
+}
+
+export default Examples;
