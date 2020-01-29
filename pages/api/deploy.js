9f169e412ac24ec64548b4b155337a4cacb280ad
Shu Uesugi <shu@chibicode.com>
2020-01-28 18:48:58 -0800

    Use await

--- pages/api/deploy.js
+++ pages/api/deploy.js
@@ -1,7 +1,7 @@
 const crypto = require('crypto');
 const fetch = require('node-fetch');
 
-export default (req, res) => {
+export default async (req, res) => {
   const hmac = crypto.createHmac('sha1', process.env.GITHUB_WEBHOOK_SECRET);
   hmac.update(JSON.stringify(req.body));
   if (req.headers['x-hub-signature'] === `sha1=${hmac.digest('hex')}`) {
@@ -9,11 +9,9 @@ export default (req, res) => {
       req.body.ref === 'refs/heads/leo-patch-1' &&
       req.body.head_commit.modified.some(x => x.startsWith('docs/'))
     ) {
-      fetch(process.env.GITHUB_WEBHOOK).then(() => res.json({}));
-    } else {
-      res.json({});
+      await fetch(process.env.GITHUB_WEBHOOK);
+      return res.json({ ok: true });
     }
-  } else {
-    res.json({});
   }
+  return res.json({});
 };
