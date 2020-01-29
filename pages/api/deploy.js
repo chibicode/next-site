c7851ad54511a820accaac64fbc72aa967bd0754
Shu Uesugi <shu@chibicode.com>
2020-01-28 18:54:27 -0800

    Use canary branch

--- pages/api/deploy.js
+++ pages/api/deploy.js
@@ -6,7 +6,7 @@ export default async (req, res) => {
   hmac.update(JSON.stringify(req.body));
   if (req.headers['x-hub-signature'] === `sha1=${hmac.digest('hex')}`) {
     if (
-      req.body.ref === 'refs/heads/leo-patch-1' &&
+      req.body.ref === 'refs/heads/canary' &&
       req.body.head_commit.modified.some(x => x.startsWith('docs/'))
     ) {
       await fetch(process.env.GITHUB_WEBHOOK);
