3a51902f2e218dffd11a23544401b3460e54ad1c
Shu Uesugi <shu@chibicode.com>
2020-01-28 17:31:44 -0800

    Update deploy.js

--- pages/api/deploy.js
+++ pages/api/deploy.js
@@ -3,6 +3,7 @@ const crypto = require('crypto');
 export default (req, res) => {
   const hmac = crypto.createHmac('sha1', process.env.secret);
   hmac.update(req.body);
+
   if (req.headers.HTTP_X_HUB_SIGNATURE === hmac.digest('hex')) {
     res.json(req.body.head_commit.modified);
   } else {
