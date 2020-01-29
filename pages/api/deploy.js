f8fd3c50e82377a1ec2262af31e7674b8af870a6
Shu Uesugi <shu@chibicode.com>
2020-01-28 18:10:31 -0800

    Lowercase

--- pages/api/deploy.js
+++ pages/api/deploy.js
@@ -4,9 +4,9 @@ export default (req, res) => {
   const hmac = crypto.createHmac('sha1', process.env.GITHUB_WEBHOOK_SECRET);
   hmac.update(JSON.stringify(req.body));
 
-  console.log(`github: ${req.headers['X-Hub-Signature']}`);
+  console.log(`github: ${req.headers['x-hub-signature']}`);
   console.log(`us: ${`sha=${hmac.digest('hex')}`}`);
-  if (req.headers['X-Hub-Signature'] === `sha=${hmac.digest('hex')}`) {
+  if (req.headers['x-hub-signature'] === `sha=${hmac.digest('hex')}`) {
     res.json(req.body.head_commit.modified);
   } else {
     res.json({});
