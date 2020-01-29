958d04736fdf2adcf321e4392d7825e13e42080d
Shu Uesugi <shu@chibicode.com>
2020-01-28 18:42:29 -0800

    Fetch github webhook

--- pages/api/deploy.js
+++ pages/api/deploy.js
@@ -1,10 +1,18 @@
 const crypto = require('crypto');
+const fetch = require('node-fetch');
 
 export default (req, res) => {
   const hmac = crypto.createHmac('sha1', process.env.GITHUB_WEBHOOK_SECRET);
   hmac.update(JSON.stringify(req.body));
   if (req.headers['x-hub-signature'] === `sha1=${hmac.digest('hex')}`) {
-    res.json(req.body.head_commit.modified);
+    if (
+      req.body.ref === 'refs/heads/leo-patch-1' &&
+      req.body.head_commit.modified.some(x => x.startsWith('docs/'))
+    ) {
+      fetch(process.env.GITHUB_WEBHOOK).then(() => res.json({}));
+    } else {
+      res.json({});
+    }
   } else {
     res.json({});
   }
