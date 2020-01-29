1c7e7d6c6245b8b06d42da27a9be8276f35235d1
Shu Uesugi <shu@chibicode.com>
2020-01-28 17:43:33 -0800

    Stringify object

--- pages/api/deploy.js
+++ pages/api/deploy.js
@@ -2,7 +2,7 @@ const crypto = require('crypto');
 
 export default (req, res) => {
   const hmac = crypto.createHmac('sha1', process.env.GITHUB_WEBHOOK_SECRET);
-  hmac.update(req.body);
+  hmac.update(JSON.stringify(req.body));
 
   if (req.headers.HTTP_X_HUB_SIGNATURE === hmac.digest('hex')) {
     res.json(req.body.head_commit.modified);
