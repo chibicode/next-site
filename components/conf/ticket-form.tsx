da8ceb29e33891886e21a9bdb1fdbbc31ad2af2a
Shusaku Uesugi <shu@chibicode.com>
2020-08-16 02:07:09 -0700

    Format ticket

--- components/conf/ticket-form.tsx
+++ components/conf/ticket-form.tsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 import cn from 'classnames';
+import GithubIcon from '@components/icons/github';
 import LoadingDots from './loading-dots';
 import formStyles from './form.module.css';
-import styles from './ticket-form.module.css';
+import ticketFormStyles from './ticket-form.module.css';
 
 type FormState = 'default' | 'loading' | 'error' | 'success';
 
@@ -12,7 +13,7 @@ export default function Form() {
   const [formState, setFormState] = useState<FormState>('default');
 
   return formState === 'error' || formState === 'success' ? (
-    <div className={styles.form}>
+    <div>
       <div className={formStyles['form-row']}>
         <div
           className={cn(formStyles['input-label'], {
@@ -43,7 +44,6 @@ export default function Form() {
     </div>
   ) : (
     <form
-      className={styles.form}
       onSubmit={e => {
         if (formState === 'default') {
           setFormState('loading');
@@ -72,25 +72,28 @@ export default function Form() {
     >
       <div className={formStyles['form-row']}>
         <label
-          htmlFor="email-input-field"
+          htmlFor="github-input-field"
           className={cn(formStyles['input-label'], {
             [formStyles.focused]: focused
           })}
         >
           <input
-            className={formStyles.input}
-            type="email"
-            id="email-input-field"
+            className={ticketFormStyles.input}
+            type="text"
+            id="github-input-field"
             value={email}
             onChange={e => setEmail(e.target.value)}
             onFocus={() => setFocused(true)}
             onBlur={() => setFocused(false)}
             disabled={false}
-            placeholder="Please enter your email…"
+            placeholder="Enter your GitHub username…"
           />
         </label>
+        <span className={ticketFormStyles.githubIcon}>
+          <GithubIcon color="var(--secondary-color)" />
+        </span>
         <button type="submit" className={cn(formStyles.submit, formStyles[formState])}>
-          {formState === 'loading' ? <LoadingDots size={4} /> : <>Register</>}
+          {formState === 'loading' ? <LoadingDots size={4} /> : <>Generate</>}
         </button>
       </div>
     </form>
