fe668a2a656e36bb177d3175d4eee9109691e1e9
Shusaku Uesugi <shu@chibicode.com>
2020-08-15 22:37:06 -0700

    Separate into components

---
+++ components/conf/hero.tsx
@@ -0,0 +1,20 @@
+import styles from './hero.module.css';
+
+export default function Hero() {
+  return (
+    <>
+      <h1 className={styles.hero}>
+        The first Next.js global
+        <br />
+        user conference
+      </h1>
+      <div className={styles.info}>
+        <div>October 27, 2020</div>
+        <div className={styles['description-separator']} />
+        <div>
+          <strong>Online</strong>
+        </div>
+      </div>
+    </>
+  );
+}
