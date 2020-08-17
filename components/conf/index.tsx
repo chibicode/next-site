edaec184a8c3a539bd6d583cdac1454c219aa8f0
Shusaku Uesugi <shu@chibicode.com>
2020-08-17 15:33:55 -0700

    Set up context

--- components/conf/index.tsx
+++ components/conf/index.tsx
@@ -1,14 +1,20 @@
+import { useState } from 'react';
+import { PageState, ConfDataContext, UserData } from '@lib/hooks/useConfData';
 import Layout from './layout';
 import Form from './form';
 import Contact from './contact';
 import Hero from './hero';
 
 export default function Conf() {
+  const [userData, setUserData] = useState<UserData>({});
+  const [pageState, setPageState] = useState<PageState>('registration');
   return (
-    <Layout>
-      <Hero />
-      <Form />
-      <Contact />
-    </Layout>
+    <ConfDataContext.Provider value={{ userData, setUserData, pageState, setPageState }}>
+      <Layout>
+        <Hero />
+        <Form />
+        <Contact />
+      </Layout>
+    </ConfDataContext.Provider>
   );
 }
