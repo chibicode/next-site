02718b7f529b1d7564699fbfd989ae25a07a8136
Shusaku Uesugi <shu@chibicode.com>
2020-08-17 21:12:34 -0700

    Fix Download URL

--- components/conf/ticket-actions.tsx
+++ components/conf/ticket-actions.tsx
@@ -8,13 +8,13 @@ export default function TicketActions({ id }: Props) {
   const permalink = encodeURIComponent(`https://nextjs.org/conf/tickets/${id}`);
   const text = encodeURIComponent('I just signed up for Next.js Conf!');
   const tweetUrl = `https://twitter.com/intent/tweet?url=${permalink}&via=vercel&text=${text}`;
-  const downloadUrl = `https://next-conf-ticket.vercel.app/ticket.png?id=${id}`;
+  const downloadUrl = `/conf/download-ticket/${id}`;
   return (
     <div>
       <a className={styles.button} href={tweetUrl} rel="noopener noreferrer" target="_blank">
         Tweet it!
       </a>
-      <a className={styles.button} href={downloadUrl} download>
+      <a className={styles.button} href={downloadUrl} download="ticket.png">
         Download
       </a>
     </div>
