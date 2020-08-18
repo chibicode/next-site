c739ff7b9e58339ec4b0ea7bd4f2b2119ee7dea1
Shusaku Uesugi <shu@chibicode.com>
2020-08-17 20:52:44 -0700

    Add action buttons

--- components/conf/ticket.tsx
+++ components/conf/ticket.tsx
@@ -4,12 +4,13 @@ import styles from './ticket.module.css';
 import layoutStyles from './layout.module.css';
 import TicketForm from './ticket-form';
 import TicketVisual from './ticket-visual';
+import TicketActions from './ticket-actions';
 
 export default function Ticket() {
   const { userData } = useConfData();
   return (
     <div className={styles['ticket-layout']}>
-      <div className={cn(styles.centered, styles['ticket-instructions'])}>
+      <div className={cn(styles['ticket-instructions'])}>
         <div className={styles['ticket-text']}>
           <h2 className={styles.hero}>
             You're in.
@@ -22,12 +23,19 @@ export default function Ticket() {
         </div>
         <TicketForm defaultUsername={userData.username} />
       </div>
-      <div className={cn(styles.centered, styles['ticket-visual'])}>
-        <TicketVisual
-          username={userData.username}
-          name={userData.name}
-          ticketNumber={userData.ticketNumber}
-        />
+      <div>
+        <div className={styles['ticket-visual']}>
+          <TicketVisual
+            username={userData.username}
+            name={userData.name}
+            ticketNumber={userData.ticketNumber}
+          />
+        </div>
+        {userData.id && userData.username && (
+          <div className={styles['ticket-actions']}>
+            <TicketActions id={userData.id} />
+          </div>
+        )}
       </div>
     </div>
   );
