8e3898eaadbec43558f14b4d76948a7f534bc4ab
Luis Alvarez <luis@vercel.com>
2020-08-19 14:32:44 -0500

    Preload the ticket

--- components/conf/loading-dots.tsx
+++ components/conf/loading-dots.tsx
@@ -1,15 +1,17 @@
 import px from '@lib/to-pixels';
+import cn from 'classnames';
 import styles from './loading-dots.module.css';
 
 interface Props {
   size?: number;
   height?: number | string;
+  reverse?: boolean;
 }
 
-const LoadingDots: React.FC<Props> = ({ size = 2, height, children }) => {
+const LoadingDots: React.FC<Props> = ({ size = 2, height, children, reverse }) => {
   return (
     <span
-      className={styles.loading}
+      className={cn(styles.loading, { [styles.reverse]: reverse })}
       style={{
         ['--loading-dots-height' as string]: height ? px(height) : undefined,
         ['--loading-dots-size' as string]: size !== 2 ? px(size) : undefined
