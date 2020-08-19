9818d0b2921fbf2bf1a399483c67b5df5836f5ad
Shusaku Uesugi <shu@chibicode.com>
2020-08-19 11:44:18 -0700

    Revert tablet

--- components/conf/hero.tsx
+++ components/conf/hero.tsx
@@ -3,23 +3,34 @@ import styleUtils from './utils.module.css';
 import styles from './hero.module.css';
 
 export default function Hero() {
+  const description = <>An interactive online experience by the community, free for everyone.</>;
   return (
     <>
       <h2
         className={cn(
           styleUtils.appear,
           styleUtils['appear-third'],
-          styleUtils['hide-on-desktop'],
+          styleUtils['show-on-mobile'],
           styles.description
         )}
       >
-        An interactive online experience by the community, free for everyone.
+        {description}
       </h2>
       <h1 className={cn(styleUtils.appear, styleUtils['appear-third'], styles.hero)}>
         The first <br className={styleUtils['show-on-tablet']} /> Next.js{' '}
         <br className={styleUtils['hide-on-tablet']} />
         global user conference
       </h1>
+      <h2
+        className={cn(
+          styleUtils.appear,
+          styleUtils['appear-third'],
+          styleUtils['hide-on-mobile'],
+          styles.description
+        )}
+      >
+        {description}
+      </h2>
       <div className={cn(styleUtils.appear, styleUtils['appear-fourth'], styles.info)}>
         <p>October 27, 2020</p>
         <div className={styles['description-separator']} />
