df6db13cc823fa337e29d2ce5f6c8b5a899c7527
Shusaku Uesugi <shu@chibicode.com>
2020-08-20 08:42:38 -0700

    Remove ticket-image/index

--- pages/conf/ticket-image/[secret].tsx
+++ pages/conf/ticket-image/[secret].tsx
@@ -13,6 +13,8 @@ export const getStaticProps: GetStaticProps<{}> = async () => {
 
 export const getStaticPaths: GetStaticPaths = async () => {
   return Promise.resolve({
+    // This page is only meant to be accessed from our API to generate the image,
+    // so protect the page path on production using the secret
     paths: [`/conf/ticket-image/${process.env.TICKET_IMAGE_SECRET || 'preview'}`],
     fallback: false
   });
--- pages/conf/ticket-image/index.tsx
+++
@@ -1,5 +0,0 @@
-import TicketImage from '@components/conf/ticket-image';
-
-export default function TicketOnlyPage() {
-  return <TicketImage />;
-}
